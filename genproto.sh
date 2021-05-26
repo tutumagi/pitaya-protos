66ba42bef6a3c453675110ae1141c2826a35be6b
tufei <tufei.fly@gmail.com>
2021-05-26 15:52:21 +0800

    把 bc 代码迁移过来时，附带的协议文件

--- genproto.sh
+++ genproto.sh
@@ -1,5 +1,9 @@
 # https://github.com/grpc/grpc-go
 # https://dev.to/toransahu/part-1-building-a-basic-microservice-with-grpc-using-golang-304d
 
+# go get google.golang.org/protobuf/cmd/protoc-gen-go@latest
+# go get -u google.golang.org/grpc
+# go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+
 # --go_out=plugins=grpc:$DST_DIR
 protoc --proto_path=./ --go_out=plugins=grpc:../ ./*.proto
