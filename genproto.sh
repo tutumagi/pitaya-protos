d8001afe3c8fc8cd2283e90b4bac64aaa9d4e4a7
tufei <tufei.fly@gmail.com>
2021-06-30 21:33:52 +0800

    disconnect 协议

--- genproto.sh
+++ genproto.sh
@@ -5,5 +5,13 @@
 # go get -u google.golang.org/grpc
 # go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
 
+# protoc-gen-go1.22 是用来业务工程那边生成proto的，用的golang.org/protobuf地址的protoc-gen-go版本，不支持下面的plugins=grpc
+# 所以这里先把文件重命名一下，用完之后再命名回去
+mv ../../../bin/protoc-gen-go ../../../bin/protoc-gen-go1.22
+mv ../../../bin/protoc-gen-go1.26 ../../../bin/protoc-gen-go
+
 # --go_out=plugins=grpc:$DST_DIR
 protoc --proto_path=./ --go_out=plugins=grpc:../ ./*.proto
+
+mv ../../../bin/protoc-gen-go ../../../bin/protoc-gen-go1.26
+mv ../../../bin/protoc-gen-go1.22 ../../../bin/protoc-gen-go
